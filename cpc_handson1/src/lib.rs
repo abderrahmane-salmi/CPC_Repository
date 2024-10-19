afd32c977b0321e2a7cd72a38d54f4916ebf5453
Abderrahmane Salmi <abderrahmane.salmi.off@gmail.com>
2024-10-19 11:49:07 +0200

    Removed more test code

--- cpc_handson1/src/lib.rs
+++ cpc_handson1/src/lib.rs
@@ -1,5 +1,4 @@
-use std::cmp::{max, min};
-use std::cell::Cell;
+use std::cmp::max;
 
 pub struct Node {
     key: u32,
@@ -87,40 +86,6 @@ impl Tree {
         0
     }
 
-    // TODO: Just for testing purposes
-    pub fn inorder(&self) {
-        self.inorder_rec(Some(0));
-    }
-
-    pub fn inorder_rec(&self, node_id: Option<usize>) {
-        if let Some(id) = node_id {
-            assert!(id < self.nodes.len(), "Node id is out of range");
-            let node = &self.nodes[id];
-
-            self.inorder_rec(node.id_left);
-            print!("{} ", node.key);
-            self.inorder_rec(node.id_right);
-        }
-
-        return;
-    }
-
-    // Sub tree size
-    pub fn subtree_size(&self, node_id: Option<usize>) -> u32 {
-        if let Some(id) = node_id {
-            assert!(id < self.nodes.len(), "Node id is out of range");
-            let node = &self.nodes[id];
-
-            let sl = self.subtree_size(node.id_left);
-            let sr = self.subtree_size(node.id_right);
-
-            let su = sl + sr + 1;
-            return su;
-        }
-
-        0
-    }
-
     // Exercise #1: Check if the binary tree is a Binary Search Tree (BST)
     pub fn is_bst(&self) -> bool {
         self.is_bst_rec(Some(0), None, None)
@@ -153,7 +118,7 @@ impl Tree {
         }
     }
 
-    // Exercise #2: Return the sum of the maximum simple path (MSP) connecting two leaves
+    // Exercise #2: Return the sum of the maximum simple path connecting two leaves
     pub fn max_path_sum(&self) -> u32 {
         let mut max_sum = u32::MIN;
         self.max_path_sum_rec(Some(0), &mut max_sum);
@@ -223,14 +188,23 @@ mod tests {
 
     #[test]
     fn test_is_bst() {
+        // Tree:
+        //        10
+        //      /    \
+        //     5      15
         let mut tree = Tree::with_root(10);
-        
         tree.add_node(0, 5, true);   // id 1
         tree.add_node(0, 15, false); // id 2
         
         assert!(tree.is_bst()); // Should be true
 
         // Add more nodes but keep the tree a BST
+        // Tree:
+        //        10
+        //      /    \
+        //     5      15
+        //    / \    /  \
+        //   2   7  12  20
         tree.add_node(1, 2, true);  // id 3
         tree.add_node(1, 7, false); // id 4
         tree.add_node(2, 12, true); // id 5
@@ -239,6 +213,14 @@ mod tests {
         assert!(tree.is_bst()); // Should be true
 
         // Add a new node that makes the tree not BST
+        // Tree:
+        //        10
+        //      /    \
+        //     5      15
+        //    / \    /  \
+        //   2   7  12  20
+        //             /
+        //            6
         tree.add_node(3, 6, true); // id 7
         
         assert!(!tree.is_bst()); // Should be false
@@ -332,21 +314,4 @@ mod tests {
         tree.add_node(2, 5, false);
         assert_eq!(tree.max_path_sum(), 0); // No path between two leaves
     }
-}
-
-fn main() {
-    println!("start main:");
-
-    let mut tree = Tree::with_root(15);
-    tree.add_node(0, 8, true); // id 1
-    tree.add_node(0, 20, false); // id 2
-
-    tree.add_node(1, 3, true); // id 3
-    tree.add_node(1, 9, false); // id 4
-
-    tree.inorder();
-    println!("\n");
-    println!("{}", tree.subtree_size(Some(1)));
-
-    println!("{}", tree.is_bst());
 }
